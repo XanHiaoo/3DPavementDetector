95051a3b89dad80d87045eb0bdbec475f5af9139
xan <1603028929@qq.com>
2023-05-19 04:50:15 +0800

    添加项目xml 输出文件属性，添加项目Manager初始化

--- 3DPavementInspectorGui/controllers/ProjectManager.h
+++ 3DPavementInspectorGui/controllers/ProjectManager.h
@@ -8,20 +8,26 @@ class ProjectManager : public QObject
     Q_OBJECT
 
 public:
-    explicit ProjectManager(QObject *parent = nullptr);
+    explicit ProjectManager(QObject* parent = nullptr);
 
     QString projectPath() const { return m_projectPath; }
     QString outputPath() const { return m_outputPath; }
+    QString projectName() const { return m_projectName; }
+    bool autoSavePointCloudDetect() const { return m_autoSavePointCloudDetect; }
 
     void setProjectPath(const QString& path) { m_projectPath = path; }
     void setOutputPath(const QString& path) { m_outputPath = path; }
+    void setProjectName(const QString& name) { m_projectName = name; }
+    void setAutoSavePointCloudDetect(bool autoSave) { m_autoSavePointCloudDetect = autoSave; }
 
 signals:
     void projectPathChanged();
 
 private:
-    QString m_projectPath;
-    QString m_outputPath;
+    QString m_projectPath = "";
+    QString m_outputPath = "";
+    QString m_projectName = "";
+    bool m_autoSavePointCloudDetect = false;
 
 signals:
 
